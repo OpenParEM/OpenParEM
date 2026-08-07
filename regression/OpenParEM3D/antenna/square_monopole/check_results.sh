3b381c8ebe8986a43dc5cfe0c0a99242e6477f52
M8kmyday <briany@austin.rr.com>
2026-08-07 14:52:43 -0500

    updated for GUI

---
+++ regression/OpenParEM3D/antenna/square_monopole/check_results.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+process3D square-planar_12GHz.proj square-planar_12GHz_test_cases.csv square-planar_12GHz_results.csv square-planar_12GHz_FarField_results.csv
+cat square-planar_12GHz_test_results.csv
+
+
